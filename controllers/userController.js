55d24d9c38c3005586bd69d58fa1319b49933ac3
Carlos <kdela.rocks@gmail.com>
2022-09-25 18:45:09 -0500

    can acces content of req.body

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ module.exports.new = (req,res) => {
 };
 
 module.exports.create = async (req,res) => {
-    const {email, password} = req.body;
-    console.log(email,password);
+    // const {email, password} = req.body;
+    // console.log(email,password);
+    console.log(req.body);
 };
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
 
 // My own routers for different parts of the app
 const authRoutes = require('./routes/authRouter')
