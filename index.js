439b19ab8cacb772d818f2ce86ffeabec89a4094
Carlos <kdela.rocks@gmail.com>
2022-09-25 18:25:22 -0500

    setup views and db

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req,res) => {
-    res.send('I will make this work no matter what')
+    res.render('signup')
 });
 
 app.listen(PORT, () => console.log('We are live on ', PORT));
